620e21c434d19c555cd50d5990cea83e7e4d9648
pupil <pupil@ivkhk.ee>
2020-09-04 15:18:45 +0300

    Продолжение проекта

---
+++ src/entity/Resource.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author pupil
+ */
+public class Resource {
+    private String url;
+    private String login;
+    private String password;
+    
+    public Resource() {
+        
+    }
+
+    public Resource(String url, String login, String password) {
+        this.url = url;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.url);
+        hash = 59 * hash + Objects.hashCode(this.login);
+        hash = 59 * hash + Objects.hashCode(this.password);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Resource other = (Resource) obj;
+        if (!Objects.equals(this.url, other.url)) {
+            return false;
+        }
+        if (!Objects.equals(this.login, other.login)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Resource{" + "url=" + url + ", login=" + login + ", password=" + password + '}';
+    }
+    
+}
--- src/jptvr18passmanager/App.java
+++ src/jptvr18passmanager/App.java
@@ -5,13 +5,56 @@
  */
 package jptvr18passmanager;
 
+import entity.Resource;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import javax.annotation.Resources;
+
 /**
  *
  * @author pupil
  */
 class App {
+    private List<Resource> listResources = new ArrayList<>();
     public void run(){
-        System.out.println("hello!");
-    }    
+        System.out.println("--- Менеджер паролей --- ");
+        String repeat = "r";
+        do{
+       
+            System.out.println("   Выберите задачу ");
+            System.out.println(" 0 Выход из программы ");
+            System.out.println(" 1 - добавить ресурс ");
+            System.out.println(" 2 - список ресурсов ");
+            Scanner scanner = new Scanner(System.in);
+            int task = scanner.nextInt();
+            scanner.nextLine();
+            switch (task) {
+                case 0:
+                    repeat = "q";
+                    break;
+                case 1:
+                    System.out.println("   Выбрана задача 1 ");
+                    System.out.println("   ________________ ");
+                    Resource resource = new Resource();
+                    resource.setUrl("http://kutsehariduskeskus.ee");
+                    resource.setLogin("admin");
+                    resource.setPassword("123123");
+                    listResources.add(resource);
+                    break;
+                case 2:
+                    System.out.println("   Выбрана задача 2 ");
+                    System.out.println("   ________________ ");
+                    System.out.println("   Список ресурсов");
+                    for(Resource r : listResources){
+                        System.out.println(r.toString());
+                    }
+                    break;
+            }
+        }while(!"q".equals(repeat));
+       
+    }
+    
 }
 
